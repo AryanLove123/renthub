73094b05ff8708e59da4548de8acd890be66a990
Lovekesh Arya <lovekesh.arya@nagarro.com>
2026-08-18 05:35:29 +0530

    designed login component

--- src/app/features/auth/pages/login/login.ts
+++ src/app/features/auth/pages/login/login.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatInputModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatButtonModule,
+  ],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  hidePassword = signal(true);
+  fb = inject(FormBuilder);
+  loginForm = this.fb.group({
+    email: ['', { validators: [Validators.required, Validators.email] }],
+    password: ['', { validators: [Validators.required] }],
+  });
+}
