8b0665b5ee286724311acb8ffe4a03f32c2f1cf0
Lovekesh Arya <lovekesh.arya@nagarro.com>
2026-08-18 06:41:50 +0530

    created the property model

---
+++ src/app/features/listings/models/property.model.ts
@@ -0,0 +1,77 @@
+export enum PropertyType {
+  Apartment = 'Apartment',
+  IndependentHouse = 'IndependentHouse',
+  Villa = 'Villa',
+  PG = 'PG',
+}
+
+export interface PropertyLocation {
+  city: string;
+  locality: string;
+  address: string;
+}
+
+export enum FurnishedStatus {
+  FullyFurnished = 'Fully Furnished',
+  SemiFurnished = 'Semi Furnished',
+  Unfurnished = 'Unfurnished',
+}
+
+export enum Leasetype {
+  ShortTerm = 'Short Term',
+  LongTerm = 'Long Term',
+}
+
+export enum VegetarianPreference {
+  Vegetarian = 'Vegetarian',
+  NonVegetarian = 'Non-Vegetarian',
+  Vegan = 'Vegan',
+}
+
+export enum PropertyStatus {
+  Available = 'Available',
+  Rented = 'Rented',
+  UnderMaintenance = 'Under Maintenance',
+}
+
+export const amenitiesList = [
+  'Gym / Fitness Center',
+  'Wi-Fi',
+  'Air Conditioning',
+  'Parking',
+  'Swimming Pool',
+  'Water Heater',
+  'Laundry Facilities',
+  'Garden',
+  'Elevator',
+  'Garbage Disposal',
+  'Security System',
+  'Clubhouse',
+];
+
+export type Amenity = typeof amenitiesList[number];
+
+export interface Property {
+  id: string;
+  propertyType: PropertyType;
+  landlordId: string;
+  isSharedProperty: boolean;
+  location: PropertyLocation;
+  areaSqft: number;
+  bedrooms: number;
+  bathrooms: number;
+  leaseType: Leasetype;
+  vegetarianPreference: VegetarianPreference;
+  propertyStatus: PropertyStatus;
+  expectedRent: number;
+  isNegotiable: boolean;
+  securityDeposit?: number;
+  furnishedStatus: FurnishedStatus;
+  description: string;
+  title: string;
+  images: string[];
+  amenities: Amenity[];
+  createdAt: string;
+  updatedAt: string;
+  availableFrom: string;
+}
